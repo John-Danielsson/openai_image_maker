476e908a603b0bc8c76111432539610fdce9f0b3
John Danielsson <johnaugustd@gmail.com>
2023-11-22 13:53:19 -0800

    add dependency injection

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -10,70 +10,85 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 class ImageGenerator:
     """
-    A class used to generate images based on prompts using OpenAI"s API.
+    A class for generating images using OpenAI's API, with capabilities for both text and audio-based prompts.
+
+    This class interfaces with OpenAI's chat and image models to generate images based on textual or spoken prompts.
+    It supports various configurations for image generation and includes methods for refining prompts and handling
+    audio inputs.
 
     Attributes
     ----------
     chat_model : str
-        The model identifier used for chat completions.
+        The identifier of the chat model used for text prompt completions (e.g., 'gpt-3.5-turbo').
+    image_model : str
+        The identifier of the image generation model used (e.g., 'dall-e-2').
     _client : openai.OpenAI
-        Client object to interact with OpenAI"s API.
-    _context : list
-        The context used for generating prompts.
+        The OpenAI client instance for API interactions.
 
     Methods
     -------
-    make_image(image_prompt, n_images, image_size=0):
-        Generates an image based on the provided prompt.
-
+    make_image_text(image_prompt, n_images, image_size):
+        Generates images based on a text prompt.
     _upgrade_prompt(prompt_text):
-        Creates and refines a prompt for image generation.
-
+        Refines a prompt text for improved image generation.
     _get_image_data(image_prompt, n_images, image_size):
-        Retrieves image data from OpenAI based on the given prompt.
-
-    make_image_with_voice(audio_file_path):
-        Generates an image based on a voice transcript.
+        Retrieves image data from OpenAI API using the given prompt.
+    make_image_voice(audio_file_path, n_images, image_size):
+        Generates images based on the transcript of an audio file.
+    _transcribe(audio_file_path):
+        Transcribes an audio file into text.
+    __valid_audio_file(audio_file_path):
+        Validates if a file path points to a valid audio file.
+    set_chat_model(chat_model):
+        Updates the chat model identifier.
+    set_image_model(image_model):
+        Updates the image model identifier.
     """
 
-    def __init__(self, chat_model="gpt-3.5-turbo", image_model="dall-e-2") -> None:
+    def __init__(self, chat_model: str, image_model: str, client: openai.OpenAI) -> None:
         """
-        Constructs all the necessary attributes for the ImageGenerator object.
+        Initializes the ImageGenerator with specified chat and image models.
 
         Parameters
         ----------
-        chat_model : str
-            The model identifier to be used for chat completions.
+        chat_model : str, optional
+            The model identifier for chat completions (default is 'gpt-3.5-turbo').
+        image_model : str, optional
+            The model identifier for image generation (default is 'dall-e-2').
         """
         self.chat_model = chat_model
         self.image_model = image_model
-        self._client = openai.OpenAI()
+        self._client = client
         self.__completions_messages = [
             {"role": "system", "content": main_context},
             {"role": "user", "content": ""}
         ]
 
-    def make_image_text(self, image_prompt, n_images, image_size=0):
+    def make_image_text(self, image_prompt: str, n_images: int, image_size: str) -> list:
         """
-        Generates an image based on the provided prompt.
+        Generates images based on a provided text prompt.
+
+        This method uses the given prompt to generate a specified number of images of a certain size.
+        It includes validation for the number of images based on the selected model.
 
         Parameters
         ----------
         image_prompt : str
-            The prompt to be used for generating the image.
+            The text prompt to use for image generation.
         n_images : int
             The number of images to generate.
-        image_size : int, optional
-            The size of the generated image (default=0).
-            Mapping and Range:
-                0: "256x256"
-                1: "512x512"
-                2: "1024x1024"
+        image_size : str
+            The size of the generated images (e.g., '1024x1024').
 
         Returns
         -------
         List[str]
-            URL of the generated image.
+            A list of URLs pointing to the generated images.
+
+        Raises
+        ------
+        ValueError
+            If n_images is not within the allowed range for the selected image model.
         """
         if n_images < 1:
             raise ValueError("n_images must be at least 1")
@@ -92,19 +107,21 @@ class ImageGenerator:
             image_urls.append(image.url)
         return image_urls
 
-    def _upgrade_prompt(self, prompt_text):
+    def _upgrade_prompt(self, prompt_text: str) -> str:
         """
-        Creates and refines a prompt for image generation.
+        Enhances a text prompt to improve the quality of generated images.
+
+        This internal method uses the chat model to refine a given prompt, making it more suitable for image generation.
 
         Parameters
         ----------
         prompt_text : str
-            The text of the prompt.
+            The original prompt text to be refined.
 
         Returns
         -------
         str
-            The refined prompt.
+            The refined prompt text.
         """
         self.__completions_messages[-1]["content"] = f"{prompt_setup} \"{prompt_text}\" {length_restriction}"
         response = self._client.chat.completions.create(
@@ -113,23 +130,25 @@ class ImageGenerator:
         )
         return response.choices[0].message.content
 
-    def _get_image_data(self, image_prompt, n_images, image_size):
+    def _get_image_data(self, image_prompt: str, n_images: int, image_size: str) -> dict:
         """
-        Retrieves image data from OpenAI based on the given prompt.
+        Fetches image data from OpenAI API based on a given prompt.
+
+        This internal method retrieves image data using the OpenAI API, given a prompt, the number of images, and their size.
 
         Parameters
         ----------
         image_prompt : str
             The prompt used for image generation.
         n_images : int
-            The number of images to generate.
-        image_size : int
-            The size of the generated image.
+            The number of images to retrieve.
+        image_size : str
+            The size of the images to be generated (e.g., '1024x1024').
 
         Returns
         -------
         dict
-            The data of the generated images.
+            A dictionary containing data of the generated images.
         """
         data = self._client.images.generate(
             prompt=image_prompt,
@@ -138,26 +157,31 @@ class ImageGenerator:
         )
         return data
 
-    def make_image_voice(self, audio_file_path, n_images, image_size=0):
+    def make_image_voice(self, audio_file_path: str, n_images: int, image_size: str) -> list:
         """
-        Generates an image based on a voice transcript from an audio file.
+        Generates images based on the transcription of an audio file.
 
-        This method transcribes the voice from the specified audio file into text 
-        and then generates an image based on this transcription. 
+        This method transcribes the content of the provided audio file and then uses the transcript as a prompt
+        for image generation. It handles both the transcription and image generation processes.
 
         Parameters
         ----------
         audio_file_path : str
-            The file path of the audio file to be transcribed and used for image generation.
+            The file path of the audio file to be transcribed.
         n_images : int
-            The number of images to generate based on the voice transcript.
-        image_size : int, optional
-            The size of the generated image (default is 0).
+            The number of images to generate from the transcript.
+        image_size : str
+            The size of the generated images (e.g., '1024x1024').
 
         Returns
         -------
-        result
-            The generated image(s) based on the voice transcript.
+        list[str]
+            A list of URLs to the generated images.
+
+        Raises
+        ------
+        ValueError
+            If the provided audio file is not valid.
         """
         audio_transcript = self._transcribe(audio_file_path)
         result = self.make_image_text(
@@ -167,22 +191,27 @@ class ImageGenerator:
         )
         return result
 
-    def _transcribe(self, audio_file_path):
+    def _transcribe(self, audio_file_path: str) -> str:
         """
         Transcribes the content of an audio file into text.
 
-        This method uses a specific model to transcribe the content of the given audio file. 
-        The transcription is used for further processing, like generating prompts for image creation.
+        This internal method uses OpenAI's transcription model to convert audio content into a text transcript.
+        It's primarily used for generating image prompts from spoken content.
 
         Parameters
         ----------
         audio_file_path : str
-            The path to the audio file that needs to be transcribed.
+            The path to the audio file to be transcribed.
 
         Returns
         -------
         str
             The transcribed text from the audio file.
+
+        Raises
+        ------
+        ValueError
+            If the audio file is not valid or cannot be transcribed.
         """
         if not self.__valid_audio_file(audio_file_path):
             raise ValueError(f"The file at \"{audio_file_path}\" is not a valid audio file.")
@@ -192,22 +221,17 @@ class ImageGenerator:
                 file=audio_file
             )
             return transcript.text
-    
-    def __valid_audio_file(self, audio_file_path):
+
+    def __valid_audio_file(self, audio_file_path: str) -> bool:
         """
-        Validates if the given file path points to a valid audio file.
+        Checks if a given file path points to a valid audio file.
 
-        This method checks whether the file exists and whether its extension
-        is one of the common audio file formats. It"s a basic validation
-        based on file existence and extension check.
-
-        Note: This method does not validate the content of the file, and 
-        a file passing this validation might still not be a proper audio file.
+        This internal method validates the existence and format of an audio file. It's a basic check based on file extension.
 
         Parameters
         ----------
         audio_file_path : str
-            The file path to be validated as an audio file.
+            The file path to be checked.
 
         Returns
         -------
@@ -217,26 +241,31 @@ class ImageGenerator:
         if not os.path.exists(audio_file_path):
             return False
         _, ext = os.path.splitext(audio_file_path)
-        return ext.lower() in ["mp3", "mp4", "mpeg", "mpga", "m4a", "wav", "webm"]
+        ext = ext[1:].lower()
+        return ext in ["mp3", "mp4", "mpeg", "mpga", "m4a", "wav", "webm"]
 
-    def set_chat_model(self, chat_model):
+    def set_chat_model(self, chat_model: str) -> None:
         """
-        Sets the chat model identifier to be used for chat completions.
+        Sets a new chat model identifier for text prompt completions.
+
+        This method updates the chat model used by the ImageGenerator instance.
 
         Parameters
         ----------
         chat_model : str
-            The model identifier (e.g., "gpt-3.5-turbo") to be used for chat completions.
+            The new chat model identifier to be set.
         """
         self.chat_model = chat_model
 
-    def set_image_model(self, image_model):
+    def set_image_model(self, image_model: str) -> None:
         """
-        Sets the image model identifier to be used for generating images.
+        Sets a new image model identifier for image generation.
+
+        This method updates the image model used by the ImageGenerator instance.
 
         Parameters
         ----------
         image_model : str
-            The model identifier (e.g., "dall-e-2", "dall-e-3") to be used for image generation.
+            The new image model identifier to be set.
         """
         self.image_model = image_model
--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 from ImageGenerator import ImageGenerator
 from test_prompts import test_prompt1, test_prompt5
+import openai
 
 if __name__ =="__main__":
-    model = "gpt-3.5-turbo"
-    # model = "gpt-4"
-    imageMaker = ImageGenerator(model)
+    chat_model = "gpt-3.5-turbo"
+    # chat_model = "gpt-4"
+    image_model = "dall-e-2"
+    # image_model = "dall-e-3"
+    client  = openai.OpenAI()
+
+    imageMaker = ImageGenerator(
+        chat_model=chat_model,
+        image_model=image_model,
+        client=client
+    )
 
     # test_text = imageMaker.make_image_text(
     #     image_prompt=test_prompt5,
@@ -12,11 +21,11 @@ if __name__ =="__main__":
     #     image_size=1
     # )
     # print(f"test_text=\"{test_text}\"")
-
-    audio_file_path = "voice_prompts/example.mp3"
-    test_voice = imageMaker.make_image_voice(
+    audio_file_path = "voice_prompts/test_voice_prompt_1.m4a"
+    image_urls = imageMaker.make_image_voice(
         audio_file_path=audio_file_path,
         n_images=2,
-        image_size=1
+        image_size="256x256"
     )
-    print(f"\n\ntest_voice=\"{test_voice}\"\n\n")
+    for i in range(len(image_urls)):
+        print(f"url {i}={image_urls[i]}\n")
