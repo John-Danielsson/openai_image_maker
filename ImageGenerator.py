0648c744da5edfb027389000b6018eefd67835c8
John Danielsson <johnaugustd@gmail.com>
2023-11-23 18:24:31 -0800

    implement async operations

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -10,11 +10,10 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 class ImageGenerator:
     """
-    A class for generating images using OpenAI's API, with capabilities for both text and audio-based prompts.
+    A class for generating images using OpenAI's API, with capabilities for both text and audio-based prompts asynchronously.
 
-    This class interfaces with OpenAI's chat and image models to generate images based on textual or spoken prompts.
-    It supports various configurations for image generation and includes methods for refining prompts and handling
-    audio inputs.
+    This class interfaces with OpenAI's chat and image models to generate images based on textual or spoken prompts asynchronously.
+    It supports various configurations for image generation and includes methods for refining prompts and handling audio inputs asynchronously.
 
     Attributes
     ----------
@@ -22,21 +21,21 @@ class ImageGenerator:
         The identifier of the chat model used for text prompt completions (e.g., 'gpt-3.5-turbo').
     image_model : str
         The identifier of the image generation model used (e.g., 'dall-e-2').
-    _client : openai.OpenAI
-        The OpenAI client instance for API interactions.
+    _client : openai.AsyncOpenAI
+        The OpenAI client instance for asynchronous API interactions.
 
     Methods
     -------
     make_image_text(image_prompt, n_images, image_size):
-        Generates images based on a text prompt.
+        Asynchronously generates images based on a text prompt.
     _upgrade_prompt(prompt_text):
-        Refines a prompt text for improved image generation.
+        Asynchronously refines a prompt text for improved image generation.
     _get_image_data(image_prompt, n_images, image_size):
-        Retrieves image data from OpenAI API using the given prompt.
+        Asynchronously retrieves image data from OpenAI API using the given prompt.
     make_image_voice(audio_file_path, n_images, image_size):
-        Generates images based on the transcript of an audio file.
+        Asynchronously generates images based on the transcript of an audio file.
     _transcribe(audio_file_path):
-        Transcribes an audio file into text.
+        Asynchronously transcribes an audio file into text.
     __valid_audio_file(audio_file_path):
         Validates if a file path points to a valid audio file.
     set_chat_model(chat_model):
@@ -45,9 +44,9 @@ class ImageGenerator:
         Updates the image model identifier.
     """
 
-    def __init__(self, chat_model: str, image_model: str, client: openai.OpenAI) -> None:
+    def __init__(self, chat_model: str, image_model: str, client: openai.AsyncOpenAI) -> None:
         """
-        Initializes the ImageGenerator with specified chat and image models.
+        Initializes the ImageGenerator with specified chat and image models for asynchronous operations.
 
         Parameters
         ----------
@@ -55,8 +54,8 @@ class ImageGenerator:
             The model identifier for chat completions (default is 'gpt-3.5-turbo').
         image_model : str
             The model identifier for image generation (default is 'dall-e-2').
-        client: openai.OpenAI
-            The OpenAI client instance for API interactions.
+        client: openai.AsyncOpenAI
+            The OpenAI client instance for asynchronous API interactions.
         """
         self.chat_model = chat_model
         self.image_model = image_model
@@ -66,12 +65,12 @@ class ImageGenerator:
             {"role": "user", "content": ""}
         ]
 
-    def make_image_text(self, image_prompt: str, n_images: int, image_size: str) -> list:
+    async def make_image_text(self, image_prompt: str, n_images: int, image_size: str) -> list:
         """
-        Generates images based on a provided text prompt.
+        Asynchronously generates images based on a provided text prompt.
 
-        This method uses the given prompt to generate a specified number of images of a certain size.
-        It includes validation for the number of images based on the selected model.
+        Uses the given prompt to asynchronously generate a specified number of images of a certain size.
+        Includes validation for the number of images based on the selected model.
 
         Parameters
         ----------
@@ -98,22 +97,20 @@ class ImageGenerator:
             raise ValueError("n_images must be 1 when self.image_model == \"dall-e-3\"")
         elif self.image_model == "dall-e-2" and n_images > 10:
             raise ValueError("n_images must be between 1 and 10 (inclusive) when self.image_model == \"dall-e-2\"")
-        upgraded_prompt = self._upgrade_prompt(image_prompt)
-        image_data = self._get_image_data(
+        upgraded_prompt = await self._upgrade_prompt(image_prompt)
+        image_data = await self._get_image_data(
             image_prompt=upgraded_prompt,
             n_images=n_images,
             image_size=image_size
         )
-        image_urls = []
-        for image in image_data.data:
-            image_urls.append(image.url)
+        image_urls = [image.url for image in image_data.data]
         return image_urls
 
-    def _upgrade_prompt(self, prompt_text: str) -> str:
+    async def _upgrade_prompt(self, prompt_text: str) -> str:
         """
-        Enhances a text prompt to improve the quality of generated images.
+        Asynchronously enhances a text prompt to improve the quality of generated images.
 
-        This internal method uses the chat model to refine a given prompt, making it more suitable for image generation.
+        Uses the chat model to asynchronously refine a given prompt, making it more suitable for image generation.
 
         Parameters
         ----------
@@ -126,17 +123,17 @@ class ImageGenerator:
             The refined prompt text.
         """
         self.__completions_messages[-1]["content"] = f"{prompt_setup} \"{prompt_text}\" {length_restriction}"
-        response = self._client.chat.completions.create(
+        response = await self._client.chat.completions.create(
             model=self.chat_model,
             messages=self.__completions_messages
         )
         return response.choices[0].message.content
 
-    def _get_image_data(self, image_prompt: str, n_images: int, image_size: str) -> dict:
+    async def _get_image_data(self, image_prompt: str, n_images: int, image_size: str) -> dict:
         """
-        Fetches image data from OpenAI API based on a given prompt.
+        Asynchronously fetches image data from OpenAI API based on a given prompt.
 
-        This internal method retrieves image data using the OpenAI API, given a prompt, the number of images, and their size.
+        Retrieves image data using the OpenAI API, given a prompt, the number of images, and their size.
 
         Parameters
         ----------
@@ -152,19 +149,19 @@ class ImageGenerator:
         dict
             A dictionary containing data of the generated images.
         """
-        data = self._client.images.generate(
+        data = await self._client.images.generate(
             prompt=image_prompt,
-        	n=n_images,
-        	size=image_size
+            n=n_images,
+            size=image_size
         )
         return data
 
-    def make_image_voice(self, audio_file_path: str, n_images: int, image_size: str) -> list:
+    async def make_image_voice(self, audio_file_path: str, n_images: int, image_size: str) -> list:
         """
-        Generates images based on the transcription of an audio file.
+        Asynchronously generates images based on the transcription of an audio file.
 
-        This method transcribes the content of the provided audio file and then uses the transcript as a prompt
-        for image generation. It handles both the transcription and image generation processes.
+        Transcribes the content of the provided audio file and then uses the transcript as a prompt for image generation.
+        Handles both the transcription and image generation processes asynchronously.
 
         Parameters
         ----------
@@ -185,20 +182,20 @@ class ImageGenerator:
         ValueError
             If the provided audio file is not valid.
         """
-        audio_transcript = self._transcribe(audio_file_path)
-        image_urls = self.make_image_text(
+        audio_transcript = await self._transcribe(audio_file_path)
+        image_urls = await self.make_image_text(
             image_prompt=audio_transcript,
             n_images=n_images,
             image_size=image_size
         )
         return image_urls
 
-    def _transcribe(self, audio_file_path: str) -> str:
+    async def _transcribe(self, audio_file_path: str) -> str:
         """
-        Transcribes the content of an audio file into text.
+        Asynchronously transcribes the content of an audio file into text.
 
-        This internal method uses OpenAI's transcription model to convert audio content into a text transcript.
-        It's primarily used for generating image prompts from spoken content.
+        Uses OpenAI's transcription model to convert audio content into a text transcript asynchronously.
+        Primarily used for generating image prompts from spoken content.
 
         Parameters
         ----------
@@ -218,7 +215,7 @@ class ImageGenerator:
         if not self.__valid_audio_file(audio_file_path):
             raise ValueError(f"The file at '{audio_file_path}' is not a valid audio file.")
         with open(audio_file_path, "rb") as audio_file:
-            transcript = self._client.audio.transcriptions.create(
+            transcript = await self._client.audio.transcriptions.create(
                 model="whisper-1", 
                 file=audio_file
             )
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 from ImageGenerator import ImageGenerator
 from test_prompts import test_prompt1, test_prompt5
 import openai
+import asyncio
 
-if __name__ =="__main__":
+async def main():
     chat_model = "gpt-3.5-turbo"
     # chat_model = "gpt-4"
     image_model = "dall-e-2"
     # image_model = "dall-e-3"
-    client  = openai.OpenAI()
+    client = openai.AsyncOpenAI()
 
     imageMaker = ImageGenerator(
         chat_model=chat_model,
@@ -15,17 +16,23 @@ if __name__ =="__main__":
         client=client
     )
 
-    # test_text = imageMaker.make_image_text(
+    # Uncomment to test text-based image generation
+    # test_text = await imageMaker.make_image_text(
     #     image_prompt=test_prompt5,
     #     n_images=2,
-    #     image_size=1
+    #     image_size="1024x1024"
     # )
     # print(f"test_text=\"{test_text}\"")
+
+    # Test audio-based image generation
     audio_file_path = "voice_prompts/test_voice_prompt_1.m4a"
-    image_urls = imageMaker.make_image_voice(
+    image_urls = await imageMaker.make_image_voice(
         audio_file_path=audio_file_path,
         n_images=2,
         image_size="256x256"
     )
-    for i in range(len(image_urls)):
-        print(f"url {i}={image_urls[i]}\n")
+    for i, url in enumerate(image_urls):
+        print(f"\nurl {i}={url}\n")
+
+if __name__ == "__main__":
+    asyncio.run(main())
